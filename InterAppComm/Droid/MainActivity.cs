3bc93feaa1d5a4aec853b57afdd5cf6d4a2e76d3
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-10-13 19:51:03 +0200

    Added part of the support for the android version of the app.

--- InterAppComm/Droid/MainActivity.cs
+++ InterAppComm/Droid/MainActivity.cs
@@ -1,13 +1,14 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Webkit;
 
 namespace InterAppComm.Droid
 {
 	[Activity (Label = "InterAppComm", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		private BehaviorSandBox obj = new BehaviorSandBox ();
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
@@ -19,9 +20,46 @@ namespace InterAppComm.Droid
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
+			WebView webView = FindViewById<WebView> (Resource.Id.webView);
 
-			button.Click += delegate { button.Text = string.Format ("{0} clicks!", count++); };
+			button.Click += delegate {
+				webView.LoadData (obj.RequestGenericIFrame (), "text/html", null);
+			};
 		}
 	}
 }
 
+/*
+using System;
+using UIKit;
+
+namespace InterAppComm.iOS
+{
+	public partial class IFrameLoaderViewController : UIViewController
+	{
+
+		private BehaviorSandBox obj = new BehaviorSandBox ();
+
+		public IFrameLoaderViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			Button.TouchUpInside += delegate {
+				WebView.LoadHtmlString (obj.RequestGenericIFrame (), null);
+			};
+
+			View.AddSubview (WebView);
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+		}
+
+
+	}
+}*/
